b76142f2dc8db566aaf72fe9e4f7c3ea64dc3947
Noah Chernet <noahchernet@yahoo.com>
2022-07-11 17:25:41 +0300

    completed webpack tutorial

--- 0x00-Webpack/tutorial/src/app.js
+++ 0x00-Webpack/tutorial/src/app.js
@@ -1,1 +1,5 @@
-console.log("Hello, Webpack!");
+import component from "./component";
+import "./style.css";
+import "./image-component";
+
+document.body.appendChild(component());
--- 0x00-Webpack/tutorial/webpack.config.js
+++ 0x00-Webpack/tutorial/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -9,9 +10,36 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "deploy"),
   },
+  // devServer: {
+  //   contentBase: "./deploy",
+  //   open: true,
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack Output",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
